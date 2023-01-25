d9b80fb7e4f45d9be6f0df002c8b07c4950634b7
Mehmet Ali Yılmaz <mehmetaliyilmaz2741@outlook.com>
2023-01-26 01:29:27 +0300

    degişken isimleri verildi

--- Program.cs
+++ Program.cs
@@ -6,26 +6,42 @@ namespace degiskenler
     {
         static void Main(string[] args)
         {
-            int deger =2;
-            string degisken = null;
+           
+           
             Console.WriteLine("Hello World!");
+
+            // byte degerler
             byte b = 5;     // 1 byte 
             sbyte c= 5;     // 1 byte
+
+            //short degerler
             short d = 7;    // 2 byte
             ushort e = 8;   // 2 byte
+
+            // int degerler
             int i = 2;      // 4 byte
             Int32 i32 = 2;  // 4 byte
             Int64 i64 = 2;  // 8 byte
             uint ui = 4;    // 4 byte
+
+            // long degerler
             long l = 5;     // 8 byte
             ulong ul = 5;   // 8 byte
+
+            // float deger
             float  f = 5; 
+
+            // double deger
             double  db = 8;  // 8 byte
+
+            // long deger
             long   dc = 6;  // 16 byte
+
+            // bool degerler
             bool b1 = true;
             bool b2 = false;
             
-            string str1 ="mehmet";
+           
              // Tarih ve saat için
             DateTime dt = DateTime.Now;
             Console.WriteLine(dt);
@@ -35,6 +51,8 @@ namespace degiskenler
             object o4 = 4.2;
             
             // string tanımlama yöntemleri
+            string degisken = null;
+            string str1 ="mehmet";
             string str2 =string.Empty;
             str2 ="mehmetali";
             string name ="enes";
@@ -42,6 +60,7 @@ namespace degiskenler
             string tanımlanmisad = name +""+lastname;
             
             // integer tanımlama yöntemleri
+            int deger =2;
             int integer1 = 5;
             int integer2 =3;
             int integer3 = integer1 + integer2;
